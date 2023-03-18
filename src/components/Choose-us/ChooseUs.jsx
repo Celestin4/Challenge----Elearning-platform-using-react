ff944daff94b746b9e1574bdb9c1fe6c6f4e971b
Celestin4 <nteziryayocelestin3@gmail.com>
2023-03-19 00:04:06 +0200

    building for deployment

--- src/components/Choose-us/ChooseUs.jsx
+++ src/components/Choose-us/ChooseUs.jsx
@@ -31,7 +31,7 @@ const ChooseUs = () => {
             <div className="choose__img">
               {showVideo ? (
                 <ReactPlayer
-                  url="https://www.youtube.com/watch?v=qFp27TR4Yew"
+                  url="https://www.youtube.com/watch?v=CkVrmLLHmuI"
                   controls
                   width="100%"
                   height="350px"
